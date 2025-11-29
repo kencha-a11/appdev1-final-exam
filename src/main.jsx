9e96b185b9be0b41f7b414b9d6e1775119ca14e5
aljon <aljonkenfernandez36@gmail.com>
2025-11-29 14:53:54 +0800

    Update readme.md and add live url

--- src/main.jsx
+++ src/main.jsx
@@ -12,7 +12,7 @@ import './assets/CSS/main.css'
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
+      <BrowserRouter basename={import.meta.env.BASE_URL}>
         <App />
       </BrowserRouter>
     </Provider>
