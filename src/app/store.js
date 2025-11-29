065594588252c5ca6064e3770f5bc034d5a47340
aljon <aljonkenfernandez36@gmail.com>
2025-11-29 12:54:06 +0800

    Implement redux store

---
+++ src/app/store.js
@@ -0,0 +1,11 @@
+// step 1 create react redux
+// next step go to main.jsx
+
+import { configureStore } from '@reduxjs/toolkit';
+import todosReducer from '../features/todos/todosSlice';
+
+export const store = configureStore({
+  reducer: {
+    todos: todosReducer,
+  },
+});
